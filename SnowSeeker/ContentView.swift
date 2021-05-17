2e988fa3f37e1d510eb1a78ccc9c73b50d4bb3c1
hvaftrudner <krill_eriksson@hotmail.com>
2021-05-17 22:58:46 +0200

    Initial Commit

---
+++ SnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Kristoffer Eriksson on 2021-05-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
